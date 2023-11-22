064ffef6a5adfbf79ff71299a165309e18dc73c4
aakashk1905 <aakashk1905@gmail.com>
2023-11-22 16:50:59 +0530

    one click register

--- src/Components/Hack/MultiStepForm.js
+++ src/Components/Hack/MultiStepForm.js
@@ -1,50 +1,58 @@
-import React, { useState } from 'react';
-import './ms.css';
-import logo from '../../Assests/logo.png'
-import close from '../../Assests/close.svg'
+import React, { useState } from "react";
+import "./ms.css";
+import logo from "../../Assests/logo.png";
+import close from "../../Assests/close.svg";
 
-const MultiStepForm = ({setShowForm}) => {
+const MultiStepForm = ({ setShowForm }) => {
   const [currentStep, setCurrentStep] = useState(1);
-  const [teamName, setTeamName] = useState('');
-  const [numMembers, setNumMembers] = useState('');
+  const [teamName, setTeamName] = useState("");
+  const [numMembers, setNumMembers] = useState("");
   const [teamMembers, setTeamMembers] = useState([{}]);
   const [teamValidation, setTeamValidation] = useState(true);
-
+  const [buttonClicked, setButtonClicked] = useState(false);
   const submitFormData = async () => {
     try {
-      const response = await fetch('https://api.tutedude.com/lms/hackathon/form', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ currentStep, teamName, numMembers, teamMembers }),
-      });
+      const response = await fetch(
+        "https://api.tutedude.com/lms/hackathon/form",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            currentStep,
+            teamName,
+            numMembers,
+            teamMembers,
+          }),
+        }
+      );
 
       if (response.status === 201) {
-        console.log('Form data saved successfully');
-        alert('Registered successfully');
+        console.log("Form data saved successfully");
+        alert("Registered successfully");
         window.location.href = "https://upskillmafia.com/success";
       } else {
-        console.error('Failed to save form data');
+        console.error("Failed to save form data");
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    if (name === 'teamName') {
+    if (name === "teamName") {
       setTeamValidation(true);
     }
-    if (name === 'numMembers' && value <= 0) {
+    if (name === "numMembers" && value <= 0) {
       setTeamValidation(false);
     }
     switch (name) {
-      case 'teamName':
+      case "teamName":
         setTeamName(value);
         break;
-      case 'numMembers':
+      case "numMembers":
         setNumMembers(value);
         break;
       default:
@@ -71,7 +79,7 @@ const MultiStepForm = ({setShowForm}) => {
   const isEmailValid = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-  }
+  };
 
   const addTeamMember = () => {
     const invalidEmails = teamMembers.filter(
@@ -79,17 +87,17 @@ const MultiStepForm = ({setShowForm}) => {
     );
 
     if (invalidEmails.length > 0) {
-      alert('Please enter Valid Email Addresses For All Members');
+      alert("Please enter Valid Email Addresses For All Members");
       return;
     }
     if (teamMembers.length < numMembers) {
       setCurrentStep(currentStep + 1);
       setTeamMembers((prevState) => [
         ...prevState,
-        { name: '', email: '', number: '' },
+        { name: "", email: "", number: "" },
       ]);
     } else {
-      console.log('Team members');
+      console.log("Team members");
       console.log({ currentStep, teamName, numMembers, teamMembers });
       submitFormData();
     }
@@ -102,24 +110,32 @@ const MultiStepForm = ({setShowForm}) => {
   };
 
   return (
-    <div className='form-container'>
-      <div className='form-logo-cont'>
-        <img src={logo} alt='Form Bg' />
-        <div className='forrm-head'>Hackathon 2023</div>
-        <div className='form-close' onClick={()=>{setShowForm(false)}}>
-          <img src={close} alt='Cross' />
+    <div className="form-container">
+      <div className="form-logo-cont">
+        <img src={logo} alt="Form Bg" />
+        <div className="forrm-head">Hackathon 2023</div>
+        <div
+          className="form-close"
+          onClick={() => {
+            setShowForm(false);
+          }}
+        >
+          <img src={close} alt="Cross" />
         </div>
       </div>
-      <div className='form-cont'>
-      <div className='form-reg-head'>Registration Form</div>
-      <div className='form-reg-head1'>(Its recommended to have 4 members it will inrease your chances of winning)</div>
+      <div className="form-cont">
+        <div className="form-reg-head">Registration Form</div>
+        <div className="form-reg-head1">
+          (Its recommended to have 4 members it will inrease your chances of
+          winning)
+        </div>
         {currentStep === 1 && (
-          <div className='f-page'>
+          <div className="f-page">
             <label>
               Team Name: *
               <input
                 type="text"
-                placeholder='Enter Team Name'
+                placeholder="Enter Team Name"
                 name="teamName"
                 value={teamName}
                 onChange={handleInputChange}
@@ -131,13 +147,23 @@ const MultiStepForm = ({setShowForm}) => {
                 type="number"
                 min={2}
                 max={4}
-                placeholder='Team Members limit 2 - 4 '
+                placeholder="Team Members limit 2 - 4 "
                 name="numMembers"
                 value={numMembers}
                 onChange={handleInputChange}
               />
             </label>
-            {!teamValidation && <p style={{ color: 'white', fontSize: '14px', alignSelf: 'center' }}>Enter Required Fields / Team members between 2-4</p>}
+            {!teamValidation && (
+              <p
+                style={{
+                  color: "white",
+                  fontSize: "14px",
+                  alignSelf: "center",
+                }}
+              >
+                Enter Required Fields / Team members between 2-4
+              </p>
+            )}
             <button type="button" onClick={handleNext}>
               Next
             </button>
@@ -145,18 +171,20 @@ const MultiStepForm = ({setShowForm}) => {
         )}
 
         {currentStep > 1 && currentStep <= numMembers + 1 && (
-          <div className='f-page'>
+          <div className="f-page">
             {/* <h2>Step {currentStep}: Team Member {currentStep - 1} Details</h2> */}
             <label>
               {currentStep - 1}st Member Name: *
               <input
                 type="text"
-                placeholder='Name Of Team Member'
+                placeholder="Name Of Team Member"
                 value={
-                  teamMembers.length !== 0 ? teamMembers[currentStep - 2].name : ''
+                  teamMembers.length !== 0
+                    ? teamMembers[currentStep - 2].name
+                    : ""
                 }
                 onChange={(e) =>
-                  updateTeamMember(currentStep - 2, 'name', e.target.value)
+                  updateTeamMember(currentStep - 2, "name", e.target.value)
                 }
               />
             </label>
@@ -164,14 +192,15 @@ const MultiStepForm = ({setShowForm}) => {
               Email: *
               <input
                 type="email"
-                placeholder='Email of team member'
-
+                placeholder="Email of team member"
                 required
                 value={
-                  teamMembers.length !== 0 ? teamMembers[currentStep - 2].email : ''
+                  teamMembers.length !== 0
+                    ? teamMembers[currentStep - 2].email
+                    : ""
                 }
                 onChange={(e) =>
-                  updateTeamMember(currentStep - 2, 'email', e.target.value)
+                  updateTeamMember(currentStep - 2, "email", e.target.value)
                 }
               />
             </label>
@@ -180,16 +209,18 @@ const MultiStepForm = ({setShowForm}) => {
               <input
                 type="number"
                 required
-                placeholder='Whatsapp Number Of Team Member'
+                placeholder="Whatsapp Number Of Team Member"
                 value={
-                  teamMembers.length !== 0 ? teamMembers[currentStep - 2].number : ''
+                  teamMembers.length !== 0
+                    ? teamMembers[currentStep - 2].number
+                    : ""
                 }
                 onChange={(e) =>
-                  updateTeamMember(currentStep - 2, 'number', e.target.value)
+                  updateTeamMember(currentStep - 2, "number", e.target.value)
                 }
               />
             </label>
-            <div className='s-page-btn-cont'>
+            <div className="s-page-btn-cont">
               {currentStep > 1 && (
                 <button type="button" onClick={handlePrevious}>
                   Previous
@@ -206,7 +237,14 @@ const MultiStepForm = ({setShowForm}) => {
                   </button>
                 )
               ) : (
-                <button type="button" onClick={addTeamMember}>
+                <button
+                  type="button"
+                  disabled={buttonClicked}
+                  onClick={() => {
+                    setButtonClicked(true);
+                    addTeamMember();
+                  }}
+                >
                   Submit
                 </button>
               )}
